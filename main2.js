582b8cf9ab13de3b7d818fad9e1447ec4e35382a
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2024-02-23 17:00:09 +0100

    ejercicio 8

--- main2.js
+++ main2.js
@@ -102,7 +102,7 @@ const conciertosRecientes = [
   }
 ];
 
-function pintaConciertos(datos){
+function pintaConciertos(datos) {
   const tbody = document.querySelector('#tbody')
 
   let tabla = ''
@@ -131,16 +131,36 @@ function pintaConciertos(datos){
 pintaConciertos(conciertosRecientes)
 // pintaConciertos()
 
+function buscaConcierto(datos, id) {
+  let div = ''
+
+  if(id < 11 || id > 20) {
+    div = 'No tengo información de conciertos para ese id'
+  } else {
+    datos = datos.find(data => data.id === id)
+    div = 
+    `
+    <p>Id: ${datos.id}</p>
+    <p>Grupo: ${datos.nombreGrupo}</p>
+    <p>Ciudad: ${datos.lugar.ciudad}</p>
+    <p>Fecha: ${datos.fecha}</p>
+    `
+  }
+  return div
+}
+
+console.log(buscaConcierto(conciertosRecientes, 14))
+document.querySelector('#infoConciertoId').innerHTML = buscaConcierto(conciertosRecientes, 22);
 
 
-// console.log(buscaConcierto(conciertosRecientes, 14))
-// document.querySelector('#infoConciertoId').innerHTML = buscaConcierto(conciertosRecientes, 14);
 
+document.querySelector('#filtrar').addEventListener('click', (e) => {
+  const ciudad = document.querySelector('#ciudad').value;
+  const info = filtraConciertos(conciertosRecientes, ciudad) 
+  pintaConciertos(info)
+  console.log('ciudad y datos concierto: ', ciudad, info);
+});
 
-
-// document.querySelector('#filtrar').addEventListener('click', (e) => {
-//   const ciudad = document.querySelector('#ciudad').value;
-//   const info = filtraConciertos(conciertosRecientes, ciudad) 
-//   pintaConciertos(info)
-//   console.log('ciudad y datos concierto: ', ciudad, info);
-// });
+function filtraConciertos(datos, ciudad) {
+  
+}
